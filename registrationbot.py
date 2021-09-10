d6551c9c9a09f5482c3e38c40cea226568c4dfa8
SavageAlex <atalaver@gmail.com>
2021-09-10 21:31:48 +0200

    Trying fix login form submit

--- registrationbot.py
+++ registrationbot.py
@@ -1,110 +1,128 @@
 from selenium import webdriver
-# import undetected_chromedriver.v2 as uc # module for solwe site Cloudflare security check
-# from seleniumwire import webdriver # webdriver for proxy authentication
-# from selenium.webdriver.common.proxy import Proxy, ProxyType
-import time
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 from proxy_connection import check_proxy_connection
+import time
+import pytz
+from datetime import datetime, timezone
+
+tz = pytz.timezone('Europe/Warsaw')
+
+# from webdriver_manager.chrome import ChromeDriverManager
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+
+USER_EMAIL = "atalaver@gmail.com"
+USER_PASSWORD = "$avageAlexVN2002"
+
+login_host = "https://kolejka-wsc.mazowieckie.pl/rezerwacje/pol/login#loc_2"
+reservation_host = "https://kolejka-wsc.mazowieckie.pl/rezerwacje/opmenus/terms/200092/200156"
+
+def check_exists_by_xpath(browser, xpath):
+    try:
+        browser.find_element(By.XPATH, xpath)
+    except NoSuchElementException:
+        return False
+    return True
+
+def make_full_screenshot(browser, screenshot_counter=0):
+    date_created = datetime.now(tz).strftime("%Y_%m_%d-%I:%M%p")
+    page_title = browser.title
+    screenshot_name = page_title.translate({ord(c): None for c in '!@#$/: '})
+    screenshot_name_datestamp = f"{date_created}_{screenshot_name}"
+    print(screenshot_name)
+    screenshot_path = f'./screenshots/{screenshot_name_datestamp}.png'
+    print(screenshot_path)
+
+    el = browser.find_element(By.TAG_NAME, 'body')
+    el.screenshot(screenshot_path)
 
 
-class RegistrationBot:
-    def __init__(self, host, headless=False, proxy_ip_port="direct://"):
-        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
-        self.host = host
-        self.headless = headless
-        
-        self.proxy_ip_port = proxy_ip_port
+def registration(headless=False, proxy_ip_port="direct://"):
 
-        if self.proxy_ip_port == "proxy.voip.plus:8080":
-            print(f'Connection with proxy: {proxy_ip_port}')
-            proxy_checked=False
-            while not proxy_checked:
-                server, str_port = proxy_ip_port.split(":")
-                port = int(str_port)
-                proxy_checked=check_proxy_connection(server, port)
-            print("Proxy connected Succesfully")     
+    screenshot_counter = 0
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
 
-        self.options = webdriver.ChromeOptions()
-        # self.options_user_data_dir = "./profile" # undetected_chromedriver.v2 
-        self.options.headless = self.headless
-        # self.options.add_argument("--no-first-run") # undetected_chromedriver.v2 
-        # self.options.add_argument("--no-service-autorun") # undetected_chromedriver.v2 
-        # self.options.add_argument("--password-store=basic") # undetected_chromedriver.v2 
-        self.options.add_argument(f'--user-agent={user_agent}')
-        self.options.add_argument("--window-size=1920,1080")
-        self.options.add_argument("--ignore-certificate-errors") # browser shows it has no this flag
-        self.options.add_argument("--allow-running-insecure-content")
-        self.options.add_argument("--disable-extensions")
-        self.options.add_argument(f'--proxy-server={self.proxy_ip_port}') # direct://
-        if self.proxy_ip_port == "direct://":
-            self.options.add_argument("--proxy-bypass-list=*")
-        else:
-            print(f'Connecting with Proxy: {self.proxy_ip_port}')
-        self.options.add_argument("--start-maximized")
-        self.options.add_argument("--disable-gpu")
-        self.options.add_argument("--disable-dev-shm-usage")
-        self.options.add_argument("--no-sandbox") # browser shows it has no this flag
-        print(self.options.arguments)
-        
-        # if self.proxy_ip_port is not None:
-        #     print(f'Connecting with Proxy: {self.proxy_ip_port}')
-        #     self.proxy = Proxy()
-        #     self.proxy.proxy_type = ProxyType.MANUAL
-        #     self.proxy.http_proxy = self.proxy_ip_port
-        #     self.proxy.ssl_proxy = self.proxy_ip_port
+    if proxy_ip_port == "proxy.voip.plus:8080":
+        print(f'Connection with proxy: {proxy_ip_port}')
+        proxy_checked=False
+        while not proxy_checked:
+            server, str_port = proxy_ip_port.split(":")
+            port = int(str_port)
+            proxy_checked=check_proxy_connection(server, port)
+        print("Proxy connected Succesfully")     
 
-        #     self.capabilities = uc.DesiredCapabilities.CHROME
-        #     self.proxy.add_to_capabilities(self.capabilities)
+    options = webdriver.ChromeOptions()
+    options.headless = headless
+    # options.add_argument("--no-first-run") # undetected_chromedriver.v2 
+    # options.add_argument("--no-service-autorun") # undetected_chromedriver.v2 
+    options.add_argument("user-data-dir=chrome_selenium_profile")
+    options.add_argument(f'--user-agent={user_agent}')
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--ignore-certificate-errors") # browser shows it has no this flag
+    options.add_argument("--allow-running-insecure-content")
+    options.add_argument("--disable-extensions")
+    options.add_argument(f'--proxy-server={proxy_ip_port}') # direct://
+    if proxy_ip_port == "direct://":
+        options.add_argument("--proxy-bypass-list=*")
+    else:
+        print(f'Connecting with Proxy: {proxy_ip_port}')
+    options.add_argument("--start-maximized")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox") # browser shows it has no this flag
+    print(options.arguments)
+    
+    chrome_service = Service(executable_path="./chromedriver")
+    browser = webdriver.Chrome(service=chrome_service, options=options)
 
-        #     self.browser = uc.Chrome(options=self.options, desired_capabilities=self.capabilities) # executable_path="./chromedriver", 
-        # else:
-        #     print("Connecting without Proxy")
-        
-        self.browser = webdriver.Chrome("./chromedriver", options=self.options)
-        
-        with self.browser:
-            self.browser.get(f'{self.host}')
-
-            time.sleep(10)
-            
-            self.screenshot_name = self.browser.title
-            print(self.screenshot_name)
-            self.screenshot_path = f'./screenshots/{self.screenshot_name}.png'
-            print(self.screenshot_path)
-
-            time.sleep(10)
-
-            # Take full page screenshot
-            self.el = self.browser.find_element_by_tag_name('body')
-            self.el.screenshot(self.screenshot_path)
-
-            self.browser.quit()        
 
     
-# class RegistrationBotUseProxy(RegistrationBot):
-#     def __init__(self, proxy_ip_port):
+    with browser:
 
-#         self.proxy_ip_port = proxy_ip_port
+        browser.get(login_host)
 
-#         proxy = Proxy()
-#         proxy.proxy_type = ProxyType.MANUAL
-#         proxy.http_proxy = self.proxy_ip_port
-#         proxy.ssl_proxy = self.proxy_ip_port
+        xpath=f"//li[@class='logo menu-acc-control']/a[@class='dropdown-button']/p[contains(text( ), '{USER_EMAIL}')]"
+        print(xpath)
+        user_is_logged = check_exists_by_xpath(browser, xpath)
 
-#         capabilities = webdriver.DesiredCapabilities.CHROME
-#         proxy.add_to_capabilities(capabilities)
+        print(user_is_logged)
+        
+        if not user_is_logged:
+
+            # Take full page screenshot
+            make_full_screenshot(browser)
+
+            time.sleep(1)
+
+            email_field = browser.find_element(By.ID, "UserEmail")
+            email_field.send_keys(USER_EMAIL)
+
+            time.sleep(1)
+
+            password_field = browser.find_element(By.ID, "UserPassword")
+            password_field.send_keys(USER_PASSWORD)
+
+            time.sleep(1)
+
+            submit_btn = browser.find_element(By.XPATH, "//input[@value='Zaloguj']")
+            submit_btn.click()
+
+            make_full_screenshot(browser)
+
+        browser.get(reservation_host)
+
+        make_full_screenshot(browser)
+
+        accept_terms = browser.find_element(By.XPATH, "//form[@id='customForm']/p/label[@for='terms']")
+        accept_terms.click()
+        accept_btn = browser.find_element(By.ID, "btn")
+        accept_btn.click()
+
+        all_acceptable_days = browser.find_elements(By.XPATH, "//div[starts-with(@id, 'zabuto_calendar_')][@class='day good']")
+        all_acceptable_days[-1].click()
+
+        make_full_screenshot(browser)
 
 
-
-
-
-RegistrationBot(host="https://kolejka-wsc.mazowieckie.pl/rezerwacje/pol/login", headless=True, proxy_ip_port="proxy.voip.plus:8080") # , headless=True, proxy_ip_port="91.149.203.12:3128"
-
-# https://whatismyipaddress.com/
-# https://kolejka-wsc.mazowieckie.pl/rezerwacje/pol/login
-# class RegistrationBot2:
-#     def __init__(self):
-#         self.browser = webdriver.Chrome("./chromedriver")
-#         self.browser.get('https://google.com')
-#         self.browser.maximize_window()
-#         print(self.browser.title)
-#         sleep(1)
+registration(headless=False, proxy_ip_port="proxy.voip.plus:8080") # , headless=True, proxy_ip_port="91.149.203.12:3128"
