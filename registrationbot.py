e3e9fe4032797fae0dd3057f08ffdf651f5bd845
SavageAlex <atalaver@gmail.com>
2021-09-11 20:06:32 +0200

    logging.ingo not INFO bag fix

--- registrationbot.py
+++ registrationbot.py
@@ -46,7 +46,7 @@ def registration(headless=False, proxy_ip_port="direct://", new_date_found=True,
 
     logging.basicConfig(level=logging_level, filename='data.log', filemode='a', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
 
-    logging.INFO("----------START----------\n")
+    logging.info("----------START----------\n")
     
     logging.info(f'New date are avalable: {new_date_found}')
 
